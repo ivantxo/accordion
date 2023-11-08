eebc631a1a7cf6f995c5b872cfb93b9e070d440d
IvanR <ivan.ramirezf@gmail.com>
2023-11-09 04:56:28 +1000

    Lifting state up
    - Accordion items remember open/close state

---
+++ src/Accordion.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import AccordionItem from "./AccordionItem";
+
+export default function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
+
+  return (
+    <div className="accordion">
+      {data.map((el, i) => (
+        <AccordionItem
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+          title={el.title}
+          num={i}
+          key={el.title}
+        >
+          {el.text}
+        </AccordionItem>
+      ))}
+    </div>
+  );
+}
---
+++ src/AccordionItem.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export default function AccordionItem({
+  curOpen,
+  onOpen,
+  num,
+  title,
+  children,
+}) {
+  const isOpen = num === curOpen;
+
+  function handleToggle() {
+    onOpen(isOpen ? null : num);
+  }
+
+  return (
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen && <div className="content-box">{children}</div>}
+    </div>
+  );
+}
